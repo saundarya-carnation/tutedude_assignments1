8a36b0838121c233be4b60878730bf8c532d2b42
saundarya-carnation <saundarya.sachan@cardinaltsinc.net>
2025-05-26 13:54:39 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,12 @@
+x = int (input("Enter first number "))
+y = int (input("Enter second number "))
+
+Addition = x + y
+Subtraction = x - y
+Multiplication = x*y
+Division = x/y
+
+print("Addition:",Addition)
+print("Substraction:",Subtraction)
+print("Multiplication:",Multiplication)
+print("Division:",Division)
---
+++ abc2.py
@@ -0,0 +1,5 @@
+x = input("Enter your first name: ")
+y = input ("Enter your last name: ")
+
+fullname = x+" "+y
+print("Hello,",fullname,"!","Welcome to the Python program")
